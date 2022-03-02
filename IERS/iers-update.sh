7fe3fb28f6f326463f71d3fbb4e60126d87bcf8b
Nick van Eijndhoven <nickve.nl@gmail.com>
2022-03-02 09:52:58 +0000

    See history for details.

---
+++ IERS/iers-update.sh
@@ -0,0 +1,26 @@
+#!bin/sh
+
+####################################################################### 
+# Shell script to update the IERS Leap Second and dUT data.
+# The data are stored in the directory $NCFS/IERS.
+#
+# At the IIHE computer cluster I have set up a personal crontab entry
+# to automatically execute this script on a daily basis at 02:00h.
+#
+# The option -N at the wget command ensures to only retrieve the file
+# when the timestamp at the server is newer than the local one.
+# This will avoid unnecessary data transport.
+#
+# Nick van Eijndhoven, IIHE-VUB, Brussel March 2, 2022  09:44Z
+#######################################################################
+
+### Go to the $NCFS/IERS directory
+cd $NCFS/IERS
+
+### Retrieve the data files from the French observatory
+wget -N https://hpiers.obspm.fr/iers/bul/bulc/Leap_Second.dat -o iers-update.log
+wget -N https://hpiers.obspm.fr/iers/series/opa/eopc04 -a iers-update.log
+
+### Copy them to our standard files leap.txt and dut.txt
+cp -f Leap_Second.dat leap.txt
+cp -f eopc04 dut.txt
