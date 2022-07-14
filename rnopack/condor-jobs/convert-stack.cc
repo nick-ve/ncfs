381e8c4124f6336c35928546093020d09d0102e0
Nick van Eijndhoven <nickve.nl@gmail.com>
2022-07-14 22:10:51 +0000

    convert-stack templates added to rnopack/condor-jobs

---
+++ rnopack/condor-jobs/convert-stack.cc
@@ -0,0 +1,151 @@
+/////////////////////////////////////////////////////////////////////////
+// ROOT macro to convert RNO-G plain ROOT data into RnoEvent structures.
+//
+// The input and output data files are specified in this ROOT macro.
+// In this template the RNO-G input data file is named "data.root" and
+// the converted output file is named "data.rnopack".
+//
+// Keeping these generic names allows the user to use this ROOT macro "as is"
+// and provide/rename the corresponding files via the convert.sub HTCondor
+// batch processing script.
+//
+// Note :
+// ------
+// By commenting out the SetOutputFile("data.rnopack") statement below,
+// the production of the rnopack output file is suppressed. 
+//
+// In this macro also the task RnoMonitor is invoked in order to
+// search for recurrent astrophysical signals due to Earth's rotation.
+// The produced monitoring histograms are written to an output file
+// which here is specified as monitor.root.
+//
+// To run this macro interactively, just do ($ is prompt)
+//
+// $root convert-stack.cc
+//
+// or for batch processing
+//
+// $root -b -q convert-stack.cc >output.log
+//
+// Note :
+// ------
+// In case the RNO-G mattak software package is not loaded, the ROOT
+// processor may issue several warnings about missing dictionaries
+// for the mattak classes.
+// Since the mattak package is not needed here, these warnings may be
+// safely ignored.
+//
+// Nick van Eijndhoven, IIHE-VUB, Brussels, July 14, 2022  16:57Z
+/////////////////////////////////////////////////////////////////////////
+{
+ gSystem->Load("ncfspack");
+ gSystem->Load("rnopack");
+
+ RnoConvert q("RnoConvert","RNO-G data to RnoEvent data structure conversion");
+
+ // Optionally limit the number of entries for testing
+ q.SetMaxEvents(-1);
+
+ // Print frequency to produce a short summary print every printfreq events
+ q.SetPrintFreq(100);
+
+ // Set the required event selection level interval for events to be written out
+ q.SetSelectLevels(0,-1);
+
+ //////////////////////////////////////////////////////
+ // The RNO-G Root data input and output filename(s) //
+ //////////////////////////////////////////////////////
+
+ // Definition of the input file(s)
+ TString dir="/pnfs/iihe/rno-g/data/2021/station22/run";
+ TString file;
+ for (Int_t irun=8; irun<=23; irun++)
+ {
+  file=dir;
+  file+=irun;
+  file+="/combined.root";
+  q.AddInputFile(file,"combined");
+ }
+ for (Int_t irun=306; irun<=309; irun++)
+ {
+  file=dir;
+  file+=irun;
+  file+="/combined.root";
+  q.AddInputFile(file,"combined");
+ }
+ for (Int_t irun=289; irun<=297; irun++)
+ {
+  file=dir;
+  file+=irun;
+  file+="/combined.root";
+  q.AddInputFile(file,"combined");
+ }
+ for (Int_t irun=284; irun<=287; irun++)
+ {
+  file=dir;
+  file+=irun;
+  file+="/combined.root";
+  q.AddInputFile(file,"combined");
+ }
+ for (Int_t irun=162; irun<=185; irun++)
+ {
+  file=dir;
+  file+=irun;
+  file+="/combined.root";
+  q.AddInputFile(file,"combined");
+ }
+
+ // Definition of the output file with the produced RnoEvent structures
+ // Comment the line below if you want to suppress producing an output file
+//// q.SetOutputFile("data.rnopack");
+
+ // Provide an overview listing of the input data chain
+///// q.ListInput("toponly");
+
+ ///////////////////////////////////////////////////////////////////
+ // Here the user can specify his/her sub-tasks to be executed
+ // on an event-by-event basis after the RnoEvent structure
+ // has been filled and before the data is written out.
+ // Sub-tasks (i.e. a user classes derived from TTask) are entered
+ // as follows :
+ //
+ //    MyClean*   task1=new MyClean("task1","Data cleaning");
+ //    MyReco*    task2=new MyReco("task2","Track reconstruction");
+ //    MyControl* task2b=new MyControl("task2b","Track quality control");
+ //    MyTagger*  task3=new MyTagger("task3","Event tagging");
+ //    MySelect*  task4=new MySelect("task4","Event selection");
+ //
+ //    task2->Add(task2b); // Make task2b a nested sub-task (of task2)
+ //
+ //    q.Add(task1);
+ //    q.Add(task2);
+ //    q.Add(task3);
+ //    q.Add(task4);
+ //
+ // The sub-tasks will be executed in the order as they are entered.
+ //
+ // Note :
+ // ------
+ // It is advised to dynamically create the various sub-tasks via the "new"
+ // constructor in order to prevent destructor issues for nested sub-tasks
+ // after completion of the ROOT macro.
+ ///////////////////////////////////////////////////////////////////
+
+ // Add a monitoring task
+ RnoMonitor* moni=new RnoMonitor();
+ moni->SetDeviceClass("RnoLPDA");
+ moni->SetSampleVariable("ADC","0.618*x-8.133");
+ moni->DefineCentralValue("RMS");
+ moni->SetNbins24(24*4);
+
+ q.Add(moni);
+
+ // Perform the conversion and execute subtasks (if any)
+ // on an event-by-event basis
+ // Invokation with the default argument is essential
+ // to obtain event-by-event processing of the sub-tasks.
+ q.ExecuteJob();
+
+ // Write the monitoring histograms to a ROOT output file
+ moni->WriteHistograms("monitor.root");
+}
---
+++ rnopack/condor-jobs/convert-stack.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+####################################################
+# Shell script to process the conversion of a RNO-G
+# plain ROOT data file into RnoEvent structures.
+#
+# This shell script is launched by the HTCondor
+# job description script convert-stack.sub.
+# See the file convert-stack.sub for further details.
+#
+# Nick van Eijndhoven, IIHE-VUB, Brussels.
+# UTC July 14, 2022  16:52
+####################################################
+
+export JOBNAME=$1
+export HOMEDIR=$2
+export HOSTDIR=$3
+export OUTPUTDIR=$4
+
+export SLAVEDIR=$_CONDOR_SCRATCH_DIR
+
+echo "*** Starting job $JOBNAME ***"
+echo " "
+echo " Jobname=$JOBNAME"
+echo " Homedir=$HOMEDIR"
+echo " Hostdir=$HOSTDIR"
+echo " Outputdir=$OUTPUTDIR"
+echo " Slavedir=$SLAVEDIR"
+echo " "
+
+cd $SLAVEDIR
+
+### Import the necessary input file(s)
+cp $HOSTDIR/convert-stack.cc macro.cc
+
+### Load the NCFS environment
+source /ice3/software/iihe/ncfs.sh
+
+### Execute the ROOT macro
+echo " "
+root -b -q macro.cc
+
+### Transfer the produced output file with RnoEvent structures
+mv -f *.rnopack $OUTPUTDIR
+
+### Transfer additionally produced histogram etc. ROOT files
+mv -f *.root $OUTPUTDIR
+
+echo " "
+echo "*** Job $JOBNAME completed ***"
